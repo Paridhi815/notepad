27fa0983ea7630b73d6e91771c8f6c2b96b528f0
paridhi <paridhi815@gmail.com>
2018-02-16 10:49:14 +0530

    Escalating the stage one level above

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -1,87 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Top from '../Title/Top';
 import Task from '../TitleContent/Task';
 import Note from '../Note/Note';
 import Bottom from '../NoteActions/Bottom';
 import './Board.css';
 
-class Board extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 120,
-      content: '',
-      inputState: '',
-      titleContent: '',
-      notes: [],
-    };
-  }
+const Board = props => (
+  <div className="board">
+    <Top
+      titleText="Note Title"
+      buttonText="en"
+    />
+    <Task
+      titleContent={event => props.titleContentSave(event)}
+      title={props.titleContent}
+    />
+    <Note
+      displayValue={props.content}
+      update={event => props.updateContent(event)}
+      inputState={props.inputState}
+    />
+    <Bottom
+      buttonText="Save"
+      remaining={props.count}
+      save={() => props.saveNote()}
+    />
+  </div>
+);
+Board.propTypes = {
+  titleContentSave: PropTypes.string.isRequired,
+  titleContent: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  updateContent: PropTypes.func.isRequired,
+  inputState: PropTypes.string.isRequired,
+  count: PropTypes.number.isRequired,
+  saveNote: PropTypes.func.isRequired,
 
-  updateContent=(event) => {
-    let text = event.target.value;
-    if (text.length > 119) {
-      text = event.target.value.slice(0, 120);
-      this.setState({
-        inputState: 'danger',
-      });
-    } else {
-      this.setState({
-        inputState: '',
-      });
-    }
-    this.setState({
-      content: text,
-      count: 120 - text.length,
-    });
-  }
-
-  titleContentSave=(event) => {
-    this.setState({
-      titleContent: event.target.value,
-    });
-  }
-
-  saveNote=() => {
-    const note = {
-      title: this.state.titleContent,
-      content: this.state.content,
-    };
-    const { notes } = this.state;
-    notes.push(note);
-    this.setState({
-      notes,
-      titleContent: '',
-      content: '',
-      count: 120,
-    });
-    console.log(this.state.notes);
-  }
-
-  render() {
-    return (
-      <div className="board">
-        <Top
-          titleText="Note Title"
-          buttonText="en"
-        />
-        <Task
-          titleContent={event => this.titleContentSave(event)}
-          title={this.state.titleContent}
-        />
-        <Note
-          displayValue={this.state.content}
-          update={event => this.updateContent(event)}
-          inputState={this.state.inputState}
-        />
-        <Bottom
-          buttonText="Save"
-          remaining={this.state.count}
-          save={() => this.saveNote()}
-        />
-      </div>
-    );
-  }
-}
-
+};
 export default Board;
 
--- src/Components/Render/Render.jsx
+++ src/Components/Render/Render.jsx
@@ -4,16 +4,114 @@ import './Render.css';
 import Header from '../Header/Header';
 import Board from '../Board/Board';
 
-const Container = () => (
-  <div className="container">
-    <Header
-      headerText="Start Taking Notes"
-    />
-    <Board />
-    <Header
-      headerText="About Us"
-    />
-  </div>
-);
+class Container extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 120,
+      content: '',
+      inputState: '',
+      titleContent: '',
+      notes: [],
+      homepage: true,
+    };
+  }
+
+  updateContent=(event) => {
+    let text = event.target.value;
+    if (text.length > 119) {
+      text = event.target.value.slice(0, 120);
+      this.setState({
+        inputState: 'danger',
+      });
+    } else {
+      this.setState({
+        inputState: '',
+      });
+    }
+    this.setState({
+      content: text,
+      count: 120 - text.length,
+    });
+  }
+
+  titleContentSave=(event) => {
+    this.setState({
+      titleContent: event.target.value,
+    });
+  }
+
+  saveNote=() => {
+    const note = {
+      title: this.state.titleContent,
+      content: this.state.content,
+    };
+    const { notes } = this.state;
+    notes.push(note);
+    this.setState({
+      notes,
+      titleContent: '',
+      content: '',
+      count: 120,
+    });
+    console.log(this.state.notes);
+  }
+
+
+  // <div className="container">
+  //   <Header
+  //     headerText="Start Taking Notes"
+  //   />
+  //   <Board />
+  //   <Header
+  //     headerText="About Us"
+  //   />
+  // </div>
+
+  render() {
+    if (this.state.homepage) {
+      return (
+        <div
+          className="Container"
+        >
+          <Header
+            headerText="Start Taking Notes"
+          />
+          <Board
+            updateTitle={receivedTitle => this.updateTitle(receivedTitle)}
+            titleText={this.state.titleContent}
+            updateFunction={content => this.updateContent(content)}
+            content={this.state.content}
+            inputClass={this.state.inputState}
+            remainingChars={this.state.count}
+            saveNote={() => this.saveNote()}
+          />
+          <Header
+            headerText="About Us"
+          />
+        </div>
+      );
+    }
+    // return (
+    //   <div className="Container">
+    //     <Header
+    //       headerText="Saved Notes"
+    //     />
+    //     <PanelList
+    //       notes={JSON.stringify(this.state.notes)}
+    //     />
+    //     <Header>
+    //       <button
+    //         className="Container-createnewnote-button"
+    //         onClick={() => this.setState({ homepage: true })}
+    //       >Create new Note
+    //       </button>
+    //     </Header>
+    //   </div>
+    // );
+  }
+}
+
+export default Container;
 
 ReactDOM.render(<Container />, document.getElementById('root'));
